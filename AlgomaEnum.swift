cb74a37b4d17e1b220f7148351a91075154f4777
harsimran31chd <harsimran.31@gmail.com>
2024-09-30 14:04:59 -0400

    Demo for Git in class

---
+++ AlgomaEnum.swift
@@ -0,0 +1,36 @@
+// enum Laptop {
+
+//   case name(String)
+//   case price (Int)
+// }
+
+// var brand = Laptop.name("APPLE")
+// print(brand)
+// var offer = Laptop.price(1599)
+// print(offer)
+///////////////////////////////////////////////////////
+// enum PizzaSize {
+//   case small, medium, large
+// }
+
+// var size = PizzaSize.medium
+
+// switch(size) {
+//   case .small:
+//     print("I ordered a small size pizza.")
+
+//   case .medium:
+//     print("I ordered a medium size pizza.")
+
+//    case .large:
+//      print("I ordered a large size pizza.");
+// }
+
+
+
+// Example: enum Barcode {
+//     case upc(Int, Int, Int, Int)
+//     case qrCode(String)
+// }
+
+// ///////////////////////////////////
